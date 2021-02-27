e0b86d64debaec310f6aff3e460b25b7a740934a
pointThink <69512713+pointThink@users.noreply.github.com>
2021-02-27 21:21:08 +0100

    Add files via upload

---
+++ Encryptor.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+using System;
+// using System.Windows.Forms;
+
+// Code by AMIDIBOSS (AQQA)
+
+public static class Encryptor
+{
+    public static string Encrypt(string clearText, string CryptoKey)
+    {
+        string EncryptionKey = CryptoKey;
+        byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+        using (Aes encryptor = Aes.Create())
+        {
+            Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+            encryptor.Key = pdb.GetBytes(32);
+            encryptor.IV = pdb.GetBytes(16);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(clearBytes, 0, clearBytes.Length);
+                    cs.Close();
+                }
+                clearText = Convert.ToBase64String(ms.ToArray());
+            }
+        }
+        return clearText;
+    }
+    public static string Decrypt(string cipherText, string CryptoKey)
+    {
+        string EncryptionKey = CryptoKey;
+        cipherText = cipherText.Replace(" ", "+");
+        byte[] cipherBytes = Convert.FromBase64String(cipherText);
+        using (Aes encryptor = Aes.Create())
+        {
+            Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+            encryptor.Key = pdb.GetBytes(32);
+            encryptor.IV = pdb.GetBytes(16);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    try {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    catch
+                    {
+                    	Console.ForegroundColor = ConsoleColor.Red;
+                    	Console.WriteLine("Could not decrypt message. Probably incorrect key");
+                    	Console.ResetColor();
+                    }
+                }
+                cipherText = Encoding.Unicode.GetString(ms.ToArray());
+            }
+        }
+        return cipherText;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * This program is based of AQQA's EasyCryptApp
+ * The Encryption code is still the same. But this time in CUI
+ * 
+ * Original program: https://github.com/AMIDIBOSS/EasyCryptApp
+ */ 
+
+// TODO: Use command-line arguments instead of program CUI
+// Will Be more efficient
+
+using System;
+
+namespace EasyCryptConsole
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("What do you want to do");
+			Console.WriteLine();
+			Console.WriteLine("1 - Encrypt");
+			Console.WriteLine("2 - Decrypt");
+			Console.WriteLine();
+			
+			string operation = Console.ReadLine();
+			Console.WriteLine();
+			
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.Write("Enter the message: ");
+			Console.ResetColor();
+			string message = Console.ReadLine();
+			
+			Console.WriteLine();
+			
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.Write("Enter the encryption key: ");
+			Console.ResetColor();
+			string key = Console.ReadLine();
+			
+			Console.WriteLine();
+			
+			if (operation == "1") { 
+				
+				Console.WriteLine("Output = " + Encryptor.Encrypt(message, key));
+			
+			} else if (operation == "2") {
+			
+				Console.WriteLine("Output = " + Encryptor.Decrypt(message, key));
+			
+			} else {
+			
+				Console.WriteLine("Invalid Operation");
+				
+			}
+			
+			Console.ReadKey(true);
+		}
+	}
+}
